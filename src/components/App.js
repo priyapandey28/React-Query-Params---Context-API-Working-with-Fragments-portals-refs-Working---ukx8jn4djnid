960ae19e928d8714ebb5195da1819fd90555444a
priyapandey28 <121967472+priyapandey28@users.noreply.github.com>
2023-01-05 00:18:14 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+import '../styles/App.css';
+
+const App = () => {
+  //Write your code here
+  const [queryParams] = useSearchParams();
+  const name = queryParams.get('name');
+  const id = queryParams.get('id');
+  const designation = queryParams.get('designation');
+
+  return <div className='details'>Employee {id} named {name} works as {designation}</div>
+}
+
+export default App;
